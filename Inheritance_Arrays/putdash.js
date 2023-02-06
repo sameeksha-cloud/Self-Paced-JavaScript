f5f09c4d0104f0f57e43860a84026b0680748c32
sameekshayadav <65281223+sameeksha-cloud@users.noreply.github.com>
2023-02-06 18:16:52 +0530

    Update putdash.js

--- Inheritance_Arrays/putdash.js
+++ Inheritance_Arrays/putdash.js
@@ -1,32 +1,27 @@
-// var input = 025468
-// var str = input.toString()
-// var res = Array.from(str)
-// var result = [res[0]]
+function DashInsert(num){
+
+    var str = num.toString()
+    //return str
+    var res = Array.from(str)
+    var result = [res[0]]
 
 
-// for(let i=1; i<str.length; i++){
+    for(let i=1; i<str.length; i++){
 
-//     if(res[i-1]%2 === 0 && res[i]%2 === 0){
-//         result.push('-',res[i])
-//     }
+        if(res[i-1]%2 === 0 && res[i]%2 === 0){
+            result.push('-',res[i])
+        }
 
-//     else{
-//         result.push(res[i])
-//     }
+        else{
+            
+            result.push(res[i])
+        }
 
-// }
+    }
 
-// console.log(result.join(''))
+    var x = result.join('')
+    return x
 
-var num = 025468
-var str = num.toString();
-var result = [str[0]];
-
-for (var x = 1; x < str.length; x++) {
-  if (str[x - 1] % 2 === 0 && str[x] % 2 === 0) {
-    result.push('-', str[x]);
-  } else {
-    result.push(str[x]);
-  }
 }
-console.log(result.join(''));
+var arresult = DashInsert(025468)
+console.log(arresult)
