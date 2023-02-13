4f1c0fb9aa88b2440060347f0e0e6e818d14703b
sameekshayadav <65281223+sameeksha-cloud@users.noreply.github.com>
2023-02-13 11:33:11 +0530

    Add files via upload

---
+++ Closure/Ques2.js
@@ -0,0 +1,5 @@
+/* 2. What is the difference between a closure and a scope ?
+
+answer
+-- Whenever you create a function within another function, then the inner function is closure. This closure is usually returned so you can use the outer function’s variables at a later time.
+-- Whereas a scope in JavaScript defines what variables you have access to. There are two kinds of scope – global scope and local scope.
